ec052d3181e6db7a14dffdf572bd8de7a3722d8a
Areeb Sheikh <as3148300@gmail.com>
2026-07-10 21:50:05 +0530

    Dock UI complete

--- my-project/src/components/Dock.jsx
+++ my-project/src/components/Dock.jsx
@@ -5,16 +5,28 @@ import gsap from "gsap";
 const Dock = () => {
   const handleEnter = (e) => {
     gsap.to(e.currentTarget, {
-     y:-10,
+      y: -10,
       duration: 0.3,
+      ease: "power2.out",
+    });
+
+    gsap.to(e.currentTarget.querySelector("p"), {
+      opacity: 1,
+      duration: .6,
       ease: "power2.out",
     });
   };
 
   const handleLeave = (e) => {
     gsap.to(e.currentTarget, {
-      y:0,
+      y: 0,
       duration: 0.3,
+      ease: "power2.out",
+    });
+
+    gsap.to(e.currentTarget.querySelector("p"), {
+      opacity: 0,
+      duration: 0.6,
       ease: "power2.out",
     });
   };
@@ -26,8 +38,41 @@ const Dock = () => {
           key={app.id}
           onMouseEnter={handleEnter}
           onMouseLeave={handleLeave}
-          className="flex flex-col items-center cursor-pointer"
+          className="relative flex flex-col items-center cursor-pointer"
         >
+          <p
+            className="
+              opacity-0
+              pointer-events-none
+              absolute
+              -top-9
+              bg-white/20
+
+
+              px-3
+              py-1
+              rounded-md
+              text-[15px]
+              font-medium
+              text-white
+              whitespace-nowrap
+
+              after:content-['']
+              after:absolute
+              after:left-1/2
+              after:top-full
+              after:-translate-x-1/2
+              after:border-l-[6px]
+              after:border-r-[6px]
+              after:border-t-[6px]
+              after:border-l-transparent
+              after:border-r-transparent
+              after:border-t-white/20
+            "
+          >
+            {app.name}
+          </p>
+
           <img
             src={`/images/${app.icon}`}
             alt={app.name}
